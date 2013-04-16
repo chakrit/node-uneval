5ffad2aaac7cb122567ed80e1efc998a22af6787
Chakrit Wichian <chakrit@oozou.com>
2013-04-16 15:02:28 +0700

    Modernize main exports file.

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
-// Public Domain
 
-// node-uneval.js - good enough approximation of uneval
-(function() {
+// node-uneval/index.js
+// ----
+// Approximation of uneval for node.js/v8.
+// Convert objects into code.
+// Don't use this unless you know all the things that could go wrong with this.
 
-  var sys = require('sys');
+module.exports = (function(undefined) {
 
-  var uneval = function (obj, ctx) {
-    var objType = typeof obj;
-      
-    // circular reference check
-    ctx = ctx || { };
-    if (ctx[obj])
-      throw "Circular references detected while uneval()-ing.";
-      
-    ctx[obj] = true;
-      
+  var util = require('util');
+
+  return function uneval(obj, known) {
+    var root = (known === undefined), result;
+    known = known || [];
+
+    // some values fail eval() if not wrapped in a ( ) parenthesises
+    var wrapRoot = function(result) {
+      return root ? ("(" + result + ")") : result;
+    }
+
     // special objects
     if (obj === null)
       return "null";
@@ -28,87 +31,36 @@
       return "-Infinity";
 
     // atoms
-    switch (objType) {
-      case 'function': return "(" + obj.toString() + ")";
+    switch (typeof obj) {
+      case 'function':
+        return wrapRoot(obj.toString());
       case 'string':
       case 'number':
-        return sys.inspect(obj);  
-    }     
-
-    // special data types
-    if (obj instanceof Array)
-      return "[" + obj.map(function(o) { return uneval(o, ctx); }).join(",") + "]";
-    
-    if (obj instanceof Date)
-      return "(new Date('" + obj.toString() + "'))";
-    
-    // complex objects
-    var key, results = [];
-    
-    for (key in obj)
-      results.push(uneval(key, ctx) + ":" + uneval(obj[key], ctx));
-      
-    return "{" + results.join(",") + "}";
-  }
-  
-  var runTests = function() {
-
-    var a = { 'value': {} };
-    var b = { 'value': {} };
-    a.value = b;
-    b.value = a;
-    circular = { 'a': a, 'b': b };
-
-    var tests = [
-      0,
-      1234,
-      01234,
-      "ASDF",
-      null,
-      undefined,
-      0xFFFF,
-      [],
-      [1,2,3],
-      [null, undefined, null, undefined, "hello"],
-      { 'x': 'y' },
-      { 'x': { 'nest': '-ed', 'y': 'z' } },
-      //circular,
-      //{ 'x': 'y', 'c': circular },
-      function() { },
-      function() { return globalVar; },
-      function(a) { return a * a; },
-      function() { return function() { }; },
-      function(a) { return function(b) { return a * b; }; },
-      new Function("return globalVar;"),
-      new Function("return function() { };"),
-      new Function("return function(a) { return a * a; };"),
-      new Function("return function() { return function() { }; };"),
-      eval("(function () { })"),
-      eval("(function () { return globalVar; })"),
-      eval("(function (a) { return a * a; })"),
-      eval("(function () { return function() { }; })"),
-      eval("(function (a) { return function(b) { return a * b; }; })"),
-      eval("(eval('(function() { })'))"),
-      eval("(eval('(function(a) { return a * a; })'))"),
-      eval("(eval('(function() { return function() { }; })'))"),
-      eval("(eval('(function(a) { return function(b) { return a * b; }; })'))"),
-      (function() { return function() { }; })(),
-      (function() { return function() { return globalVar; }; })(),
-      (function(a) { return function() { a * a; }; })(),
-      { 'x': 'a', 'func': (function() { }) },
-    ];  
-    
-    var results = tests.map(uneval);
-    
-    for (var i = 0; i < tests.length; i++) {
-      sys.puts(i.toString() + " : " + results[i]);
+        return util.inspect(obj);
     }
+
+    // circular reference check for non-atoms
+    if (known.indexOf(obj) !== -1)
+      throw new Error("Circular references detected while uneval()-ing.");
+
+    known.push(obj);
+
+    // specialized types
+    if (obj instanceof Array)
+      return "[" + obj.map(function(o) { return uneval(o, known); }).join(",") + "]";
+
+    if (obj instanceof Date)
+      return wrapRoot("new Date('" + obj.toString() + "')");
+
+    // hashes
+    var key, pairs = [];
+
+    for (key in obj)
+      pairs.push(uneval(key, known) + ":" + uneval(obj[key], known));
+
+    return wrapRoot("{" + pairs.join(",") + "}");
+
   };
-  
-  exports.uneval = uneval;
-  exports.runTests = runTests;
-  
-  if (module.parent === undefined)
-    runTests();
 
 })();
+
